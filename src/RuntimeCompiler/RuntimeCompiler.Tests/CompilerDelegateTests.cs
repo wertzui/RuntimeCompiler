2542d3febfdddce49a0216655a3c574d84bc8a0f
Andre Hähnel <5433898+anhaehne@users.noreply.github.com>
2022-08-16 14:36:28 +0200

    Added support for the in, out and ref modifiers (#2)
    Co-authored-by: Andre Hähnel <andre.haehnel@guenstiger.de>

--- src/RuntimeCompiler/RuntimeCompiler.Tests/CompilerDelegateTests.cs
+++ src/RuntimeCompiler/RuntimeCompiler.Tests/CompilerDelegateTests.cs
@@ -19,6 +19,56 @@ namespace RuntimeCompiler.Tests
             Assert.AreEqual(expectedResult, actualResult);
         }
 
+        [TestMethod]
+        public void Custom_delegate_supports_in_modifier()
+        {
+            // Arrange
+            var expectedValue = 13;
+
+            // Act
+            var func = Compiler.CompileDelegate<MyInDelegate>("value", new[] { "value" });
+            var result = func(in expectedValue);
+
+            // Assert
+            Assert.AreEqual(expectedValue, result);
+        }
+
+        [TestMethod]
+        public void Custom_delegate_supports_out_modifier()
+        {
+            // Arrange
+            var expectedValue = 13;
+
+            // Act
+            var func = Compiler.CompileDelegate<MyOutDelegate>("value = 13;", new[] { "value" });
+            func(out var result);
+
+            // Assert
+            Assert.AreEqual(expectedValue, result);
+        }
+
+        [TestMethod]
+        public void Custom_delegate_supports_ref_modifier()
+        {
+            // Arrange
+            var expectedValue = 13;
+            var result = 12;
+
+            // Act
+            var func = Compiler.CompileDelegate<MyRefDelegate>("value++;", new[] { "value" });
+            func(ref result);
+
+            // Assert
+            Assert.AreEqual(expectedValue, result);
+        }
+
+
         delegate string MyDelegate(string value);
+
+        delegate int MyInDelegate(in int value);
+
+        delegate void MyOutDelegate(out int value);
+
+        delegate void MyRefDelegate(ref int value);
     }
 }
